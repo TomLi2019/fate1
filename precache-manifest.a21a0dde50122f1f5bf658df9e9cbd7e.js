507950d710a2e47f524748db385d6a69de944744
tomli2019 <zuomingli@gmail.com>
2020-04-11 12:55:04 -0400

    Updates

---
+++ precache-manifest.a21a0dde50122f1f5bf658df9e9cbd7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5bd47cda6a125a0f03be23e1eb3ac3",
+    "url": "/fate1/index.html"
+  },
+  {
+    "revision": "11ab8532201a50bb4843",
+    "url": "/fate1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de71e1cd74fbc2076777",
+    "url": "/fate1/static/js/2.bc195109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fate1/static/js/2.bc195109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ab8532201a50bb4843",
+    "url": "/fate1/static/js/main.0a297316.chunk.js"
+  },
+  {
+    "revision": "e1f88cea8c7ec9075399",
+    "url": "/fate1/static/js/runtime-main.b2b3d78d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fate1/static/media/logo.5d5d9eef.svg"
+  }
+]);
